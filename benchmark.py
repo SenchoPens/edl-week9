78ba03c3a1027dafe97377c4a595bf584ad77c25
Arseniy Chekanov <senya@chekanov.net>
2024-03-30 04:45:52 +0300

    ft1

--- benchmark.py
+++ benchmark.py
@@ -15,7 +15,7 @@ def benchmark(model, sample_inputs, batch_size):
 
 
 if __name__ == "__main__":
-    sample_inputs = np.random.randn(32, 3, 224, 224).astype(np.float32)
+    sample_inputs = np.random.randn(32, 3, 32, 32).astype(np.float32)
     benchmark('checkpoints/baseline_resnet.onnx', sample_inputs, 1)
     benchmark('checkpoints/baseline_resnet.onnx', sample_inputs, 32) 
     benchmark('pruned_quantized_resnet.onnx', sample_inputs, 1)
